fb86cfb12cf3426994d02205ff660fcf6cb0eac0
kieranwoodrow <kwoodrow@tracker.co.za>
2024-03-04 11:43:09 +0200

    added list

--- test2/test2/ContentView.swift
+++ test2/test2/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        List {
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
+            Text("Hello, world!")
             Text("Hello, world!")
         }
-        .padding()
     }
 }
 
