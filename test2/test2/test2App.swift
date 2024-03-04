0237354265693c107774526977552fc439578391
kieranwoodrow <kwoodrow@tracker.co.za>
2024-03-04 11:29:41 +0200

    test

---
+++ test2/test2/test2App.swift
@@ -0,0 +1,17 @@
+//
+//  test2App.swift
+//  test2
+//
+//  Created by Kieran Woodrow on 2024/03/04.
+//
+
+import SwiftUI
+
+@main
+struct test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
