a74324b3ae587809d40328b916ae67fba31f7cb0
Kieran Woodrow <77970893+kieran-woodrow@users.noreply.github.com>
2024-03-04 14:47:24 +0200

    new bugfixes (#4)
    * added list
    
    * Bug fix/123456 rename the list items (#3)
    
    * fixed List item 1
    
    * fixed list item 2
    
    * fixed all the list items
    
    ---------
    
    Co-authored-by: kieranwoodrow <kwoodrow@tracker.co.za>
    
    ---------
    
    Co-authored-by: kieranwoodrow <kwoodrow@tracker.co.za>

--- test2/test2/ContentView.swift
+++ test2/test2/ContentView.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List {
-            Text("Hello, world!")
-            Text("Hello, world!")
-            Text("Hello, world!")
-            Text("Hello, world!")
-            Text("Hello, world!")
+            Text("Hello Seb!")
+            Text("Hello Gia!")
+            Text("Hello Apiwhe!")
+            Text("Hello k1!")
+            Text("Hello k2!")
         }
          .padding()
     }
